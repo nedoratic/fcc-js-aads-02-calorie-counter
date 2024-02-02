25e9fb0cb814f79524188e2e001adac979352dc5
nedoratic <nedoratic@gmail.com>
2024-02-02 16:27:07 +0100

    cleanInputString function added

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
+
+function cleanInputString(str) {
+	const regex = /[+-\s]/g;
+	return str.replace(regex, "");
+}
