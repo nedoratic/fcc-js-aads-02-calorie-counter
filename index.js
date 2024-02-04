d054d0f1615af496b949e8453336251a0aac3a2e
nedoratic <nedoratic@gmail.com>
2024-02-04 20:07:12 +0100

    code comments added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// DOM elements
+// DOM Elements
 const calorieCounter = document.getElementById("calorie-counter");
 const budgetNumberInput = document.getElementById("budget");
 const entryDropdown = document.getElementById("entry-dropdown");
@@ -7,11 +7,13 @@ const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
 
+// Clean Input String
 function cleanInputString(str) {
 	const regex = /[+-\s]/g;
 	return str.replace(regex, "");
 }
 
+// Check for Invalid String
 function isInvalidInput(str) {
 	const regex = /\d+e\d+/i;
 	return str.match(regex);
