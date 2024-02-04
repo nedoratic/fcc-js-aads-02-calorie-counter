e9de18af51b6ae2200d69cb03ca48313e0492797
nedoratic <nedoratic@gmail.com>
2024-02-04 22:04:46 +0100

    getCaloriesFromInputs function added

--- index.js
+++ index.js
@@ -25,11 +25,30 @@ function addEntry() {
 		`#${entryDropdown.value} .input-container`
 	);
 	const entryNumber =
-		targetInputContainer.querySelectorAll('input[type="text"]').length;
+		targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
 	const HTMLString = `
-	<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-	<input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-	<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-	<input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"/>`;
-	targetInputContainer.innerHTML += HTMLString;
+  		<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  		<input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  		<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  		<input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"/>`;
+	targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
+// Get Calories from Inputs
+function getCaloriesFromInputs(list) {
+	let calories = 0;
+	for (let i = 0; i < list.length; i++) {
+		const currVal = cleanInputString(list[i].value);
+		const invalidInputMatch = isInvalidInput(currVal);
+		if (invalidInputMatch) {
+			alert(`Invalid Input: ${invalidInputMatch[0]}`);
+			isError = true;
+			return null;
+		}
+		calories += Number(currVal);
+	}
+	return calories;
+}
+
+// Add Entry Button Event Listener
+addEntryButton.addEventListener('click', addEntry);
