0aee854414787905932edbfa1e31f94bf9142a1b
nedoratic <nedoratic@gmail.com>
2024-02-04 20:39:38 +0100

    addEntry function added

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // DOM Elements
-const calorieCounter = document.getElementById("calorie-counter");
-const budgetNumberInput = document.getElementById("budget");
-const entryDropdown = document.getElementById("entry-dropdown");
-const addEntryButton = document.getElementById("add-entry");
-const clearButton = document.getElementById("clear");
-const output = document.getElementById("output");
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
 let isError = false;
 
 // Clean Input String
 function cleanInputString(str) {
 	const regex = /[+-\s]/g;
-	return str.replace(regex, "");
+	return str.replace(regex, '');
 }
 
 // Check for Invalid String
@@ -19,4 +19,17 @@ function isInvalidInput(str) {
 	return str.match(regex);
 }
 
-function addEntry() {}
+// Add Entry
+function addEntry() {
+	const targetInputContainer = document.querySelector(
+		`#${entryDropdown.value} .input-container`
+	);
+	const entryNumber =
+		targetInputContainer.querySelectorAll('input[type="text"]').length;
+	const HTMLString = `
+	<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+	<input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+	<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+	<input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"/>`;
+	targetInputContainer.innerHTML += HTMLString;
+}
